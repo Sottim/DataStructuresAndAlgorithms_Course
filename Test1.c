1b6db1d8829c73bb272091c2bc9a183e18493a52
Sottim <106998247+Sottim@users.noreply.github.com>
2022-10-02 00:17:57 +0530

    Change

--- Test1.c
+++ Test1.c
@@ -5,10 +5,12 @@
 int main(int argc, char **argv) {
     
     int i;
+    int count= 0;
     puts("Printing:");
     for(i = 1; i <= 10; i++) {
         printf("%d\n", i);
+        count = count + 1;
     }
-
+    printf("%d\n", count);
     return 0;
 }
