a8a0b9891417b5bbb4d39638e0c93f386e2190d7
Sottim <106998247+Sottim@users.noreply.github.com>
2022-10-01 23:58:28 +0530

    First commit

--- Test1.c
+++ Test1.c
@@ -5,15 +5,11 @@
 int main(int argc, char **argv) {
     
     int i;
-    int count = 0;
 
     puts("Printing:");
     for(i = 1; i <= 10; i++) {
         printf("%d\n", i);
-        count = count + 1;
     }
-    printf("%d\n", count);
-    
 
     return 0;
 }
