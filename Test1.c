28e056f81d214deb145747e6cdb8247ba0ca1464
Sottim <106998247+Sottim@users.noreply.github.com>
2022-10-01 23:36:59 +0530

    Added the counter.

---
+++ Test1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/* print integers from 1 to 5 */
+
+int main(int argc, char **argv) {
+    
+    int i;
+    int count = 0;
+
+    puts("Printing:");
+    for(i = 1; i <= 10; i++) {
+        printf("%d\n", i);
+        count = count + 1;
+    }
+    printf("%d\n", count);
+    
+
+    return 0;
+}
