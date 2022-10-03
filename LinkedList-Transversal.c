8fff8a729182ce858e6e7e4978c95b49ab6cd994
Sottim <106998247+Sottim@users.noreply.github.com>
2022-10-03 23:06:14 +0530

    Transversal of the elements of the Nodes in Linked List

---
+++ LinkedList-Transversal.c
@@ -0,0 +1,37 @@
+#include <stdio.h> // Simple Linked list with 3 Nodes.
+#include <stdlib.h>
+
+//https://stackoverflow.com/questions/69975191/how-does-struct-node-ptr-mallocsizeofptr-work
+
+struct Node{ // An object of the type struct Node is allocated a memory.
+    int data;
+    struct Node *next;
+};
+
+void ListTransversal(struct Node *address){
+    while(address != NULL){
+        printf("Elements = %d\n", address->data);
+        address = address->next;
+    }
+}
+
+int main () {
+    struct Node *head = NULL; // Initialize address of all is declared as NULL
+    struct Node *second = NULL;
+    struct Node *third = NULL;
+
+    head = (struct Node*)(malloc(sizeof(struct Node*)));
+    second = (struct Node*)(malloc(sizeof (struct Node))); //malloc() allocates the specific size in the memory and second finally would contain the address of the second node.
+    third = (struct Node*)(malloc(sizeof(struct Node)));
+
+    head-> data = 10; // the data stored in the first node is 10
+    head->next = second; //contaions the address of the next node
+    second->data = 20;
+    second->next = third;
+    third->data = 30;
+    third->next = NULL;
+
+    ListTransversal(head);// function call with address of head as a parameter
+
+    return 0;
+}
