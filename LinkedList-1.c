7263956d65e76354bb4556190b2719f83ec34289
Sottim <106998247+Sottim@users.noreply.github.com>
2022-10-03 22:37:18 +0530

    One by one, print all the elements using printf()

--- LinkedList-1.c
+++ LinkedList-1.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-struct Node{
+//https://stackoverflow.com/questions/69975191/how-does-struct-node-ptr-mallocsizeofptr-work
+struct Node{ // An object of the type struct Node is allocated a memory.
     int data;
     struct Node *next;
 };
 
 int main () {
-    struct Node *head = NULL;
+    struct Node *head = NULL; // Initialize address of all is declared as NULL
     struct Node *second = NULL;
     struct Node *third = NULL;
 
@@ -15,13 +15,15 @@ int main () {
     second = (struct Node*)(malloc(sizeof (struct Node))); //malloc() allocates the specific size in the memory and second finally would contain the address of the second node.
     third = (struct Node*)(malloc(sizeof(struct Node)));
 
-    head-> data = 10;
-    head->next = second;
+    head-> data = 10; // the data stored in the first node is 10
+    head->next = second; //contaions the address of the next node
     second->data = 20;
     second->next = third;
     third->data = 30;
     third->next = NULL;
-    printf("First element: %d", head->data);
+
+    printf("First Element: %d\n", head->data); //One by one tech. to print elements in diff. nodes (which is inefficient)
+    printf("Second Ele: %d\n", second->data);
     
     return 0;
 }
